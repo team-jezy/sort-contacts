dfa36abeaf023654073dae3949f91bc67ffa8573
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-24 13:46:40 -0700

    build out initial files and grab contacts data

---
+++ src/html-to-dom.js
@@ -0,0 +1,9 @@
+function htmlToDOM(html) {
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const content = template.content;
+    const firstElementChild = content.firstElementChild;
+    return firstElementChild;
+}
+
+export default htmlToDOM;
